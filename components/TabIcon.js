364346fbdc0326fc828a949e5d3a499d8ead1d17
prisnguyen <112917413+prisnguyen@users.noreply.github.com>
2022-10-10 22:17:42 -0400

    Add files via upload

---
+++ components/TabIcon.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { COLORS } from "../constants"
+
+const TabIcon = ({focused, icon}) => {
+    return (
+        <View
+            style={{
+                alignItems: 'center',
+                height: 40,
+                width: 50,
+                marginTop: 15
+            }}
+        >
+            <Image
+                source={icon}
+                resizeMode="contain"
+                style={{
+                    width: 30,
+                    height: 30,
+                    tintColor: focused ? COLORS.darkGreen : COLORS.lightLime
+                }}
+            />
+
+            {focused &&
+                <View
+                    style={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            height: 5,
+                            borderTopLeftRadius: 5,
+                            borderTopRightRadius: 5,
+                            backgroundColor: COLORS.darkGreen
+                    }}
+                />
+            }
+        </View>
+    )
+}
+
+export default TabIcon;
---
+++ components/index.js
@@ -0,0 +1,11 @@
+import CategoryCard from "./CategoryCard";
+import CustomButton from "./CustomButton";
+import TabIcon from "./TabIcon";
+import TrendingCard from "./TrendingCard";
+
+export {
+    CategoryCard,
+    CustomButton,
+    TabIcon,
+    TrendingCard
+}
